6af9bf7f0719e7438a711c876465c358023eafb1
Craig Anderson <canderson@arcresources.com>
2010-10-18 11:37:27 -0600

    added validation.

--- src/MVPtoMVVM.mvvm/viewmodels/ToDoItemViewModel.cs
+++ src/MVPtoMVVM.mvvm/viewmodels/ToDoItemViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
-using System.Windows.Input;
+using System.Text;
 using MVPtoMVVM.domain;
 using MVPtoMVVM.repositories;
+using System.Linq;
 
 namespace MVPtoMVVM.mvvm.viewmodels
 {
-    public class TodoItemViewModel : INotifyPropertyChanged
+    public class TodoItemViewModel : INotifyPropertyChanged, IDataErrorInfo
     {
         private readonly ITodoItemRepository todoItemRepository;
         private Synchronizer<TodoItemViewModel> updater;
@@ -17,6 +19,11 @@ namespace MVPtoMVVM.mvvm.viewmodels
             SaveCommand = new SimpleCommand(Save, CanSave);
             DeleteCommand = new SimpleCommand(Delete);
             updater = new Synchronizer<TodoItemViewModel>(PropertyChanged);
+            validations = new Dictionary<string, IValidation>
+                              {
+                                  {"Description", new Validation(() => !string.IsNullOrEmpty(Description), "Cannot have an empty description.")},
+                                  {"DueDate", new Validation(() => DueDate >= DateTime.Now, "Due Date must occur on or after today.")}
+                              };
         }
 
         private void Delete()
@@ -28,7 +35,7 @@ namespace MVPtoMVVM.mvvm.viewmodels
 
         private bool CanSave()
         {
-            return !string.IsNullOrEmpty(Description) && DueDate >= DateTime.Today;
+            return validations.Values.All(x => x.IsValid);
         }
 
         private void Save()
@@ -54,6 +61,8 @@ namespace MVPtoMVVM.mvvm.viewmodels
         }
 
         private DateTime dueDate;
+        private IDictionary<string, IValidation> validations;
+
         public DateTime DueDate
         {
             get { return dueDate; }
@@ -66,5 +75,32 @@ namespace MVPtoMVVM.mvvm.viewmodels
         public IObservableCommand SaveCommand { get; set; }
         public IObservableCommand DeleteCommand { get; set; }
         public MainWindowViewModel Parent { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                var validation = validations[columnName];
+                return validation.IsValid ? null : validation.Message;
+            }
+        }
+
+        public string Error
+        {
+            get { return BuildErrors(); }
+        }
+
+        private string BuildErrors()
+        {
+            var builder = new StringBuilder();
+            foreach (var validation in validations.Values)
+            {
+                if(!validation.IsValid)
+                {
+                    builder.AppendLine(validation.Message);
+                }
+            }
+            return builder.ToString();
+        }
     }
 }
---
+++ src/MVPtoMVVM.mvvm/viewmodels/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MVPtoMVVM.mvvm.viewmodels
+{
+    class Validation : IValidation
+    {
+        private Func<bool> Condition;
+
+        public Validation(Func<bool> condition, string message)
+        {
+            Condition = condition;
+            Message = message;
+        }
+
+        public bool IsValid
+        {
+            get { return Condition(); }
+        }
+
+        public string Message { get;set;}
+    }
+}
